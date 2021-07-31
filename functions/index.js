1166b7a0fb35b49944ed0ee470106934beebadb7
new5558 <new17353@gmail.com>
2021-07-31 22:18:12 +0700

    :tada: initialize project

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({ original: original });
+    // Send back a message that we've successfully written the message
+    res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
