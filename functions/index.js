4e60625e2d192b85a2e92199c4f09783ea04991a
palsp <palsp2406@gmail.com>
2021-08-03 01:20:16 +0700

    delete: addMessage

--- functions/index.js
+++ functions/index.js
@@ -20,20 +20,6 @@ const app = express();
 initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions
-  .region(region)
-  .https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin
-      .firestore()
-      .collection("messages")
-      .add({ original: original });
-    // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-  });
 
 exports.registerParticipant = functions
   .region(region)
