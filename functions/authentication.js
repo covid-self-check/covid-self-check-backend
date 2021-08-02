66c0b7e73bbd12806ed685e068af06807d412154
new5558 <new17353@gmail.com>
2021-08-02 16:05:41 +0700

    feat: add getProfile endpoint

--- functions/authentication.js
+++ functions/authentication.js
@@ -1,11 +1,12 @@
 const { admin } = require('./init');
+const axios = require('axios')
 
 /**
- * Authenticate middleware
+ * Authenticate middleware for volunteer system
  * @param {*} func function to call if authenticate success
  * @returns error 401 if not authorized email
  */
-exports.authenticate = (func) => {
+exports.authenticateVolunteer = (func) => {
     return async (data, context) => {
         if (!context.auth) return { status: 'error', code: 401, message: 'Not signed in' }
         const email = context.auth.token.email || null;
@@ -15,4 +16,24 @@ exports.authenticate = (func) => {
         }
         return await func(data, context)
     }
+}
+
+
+exports.getProfile = async (data) => {
+    const { lineIDToken, userID } = data;
+
+    const params = new URLSearchParams()
+    params.append("client_id", functions.config().liff.channelid)
+    params.append("id_token", lineIDToken)
+    params.append("user_id", userID)
+
+    const response = await axios.post('https://api.line.me/oauth2/v2.1/verify', params, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    console.log(response, 'res')
+
+    const userProfile = { ...response }
+    return userProfile
 }
