cad3d0ac31f4a94bf2ecd54f69cd4dc140a2753b
guynawapas <guynawapas@gmail.com>
2021-08-02 15:39:06 +0700

    greenDone

--- functions/index.js
+++ functions/index.js
@@ -100,6 +100,84 @@ exports.exportPatientData = functions
     await exportPatient(id, documentData);
   });
 
+
+function calculateStatus(snapshot,currentSymptom){
+  console.log(isGreen(snapshot,currentSymptom));
+  
+  
+ 
+  
+  
+}
+//status 0
+function isGreen(snapshot,currentSymptom){
+  const data = snapshot.data();
+  const age = data.age;
+  const diseases = data.congenitalDisease;
+  const diseaseList = diseases.split(/[ ,]+/);
+  var ok = true;
+  const shouldBeFalse = ['cough','runnynose','redEye','rash','soreThroat','canNotSmell','canNotTaste','diarrhoeaMoreThan3','tired','stuffyChest','nausea','chestHurt','slowResponse','headAche'];
+  const shouldBeTrue = ['canBreathRegularly']
+  const shouldNotHaveCongenitalDisease = ['ปอด','หอบ','หลอดลม','ถุงลมโป่งพอง','ไต','หัวใจ','หลอดเลือด','อัมพาต','อัมพฤกษ์','เส้นเลือดในสมอง','ความดัน','ไขมัน','เบาหวาน','ภูมิคุ้มกัน','ตับ','LSD','มะเร็ง'];
+  
+  if(age>5&&age<60&&currentSymptom.bodyTemperature<37){
+   shouldBeFalse.every(symptom=>{
+     if (currentSymptom[symptom]){
+      ok = false;
+      return false;
+     }
+   })
+   shouldBeTrue.every(symptom=>{
+    if (!currentSymptom[symptom]){
+      ok = false;
+      return false;
+     }
+   })
+   diseaseList.forEach(disease=>{
+    shouldNotHaveCongenitalDisease.forEach(checkList=>{
+      if(disease.includes(checkList)){
+        ok = false;
+      }
+    })
+   })
+  }else{
+    return false;
+  }
+  return ok;
+
+
+}
+function isGreenWithSymptom(snapshot,currentSymptom){
+  const data = snapshot.data();
+  const age = data.age;
+  const diseases = data.congenitalDisease;
+  const diseaseList = diseases.split(/[ ,]+/);
+  const ok = true;
+  const shouldBeFalse = ['cough','runnyNose','redEye','rash','soreThroat','canNotSmell','canNotTaste','diarrhoeaMoreThan3','tired','stuffyChest','nausea','chestHurt','slowResponse','headAche']
+  const shouldBeTrue = ['canBreathRegularly']
+  if(age>5&&age<60){
+   shouldBeFalse.every(symptom=>{
+     if (currentSymptom[symptom]){
+      ok = false;
+      return false;
+     }
+   })
+   shouldBeTrue.every(symptom=>{
+    if (!currentSymptom[symptom]){
+      ok = false;
+      return false;
+     }
+   })
+  }
+  return ok;
+}
+function isYellow(snapshot,currentSymptom){
+  
+}
+function isRed(snapshot,currentSymptom){
+  
+}
+
 exports.updateSymptom = functions
   .region(region)
   .https.onCall(async ( data ) => {
@@ -126,6 +204,8 @@ exports.updateSymptom = functions
         `ไม่พบผู้ใช้ ${lineId}`
       );
     }
+    
+    snapshot.data().status = calculateStatus(snapshot,obj);
 
     const { followUp } = snapshot.data();
     
--- functions/schema/HistorySchema.js
+++ functions/schema/HistorySchema.js
@@ -6,8 +6,25 @@ module.exports = Joi.object({
    bodyTemperature : Joi.number().required(),
    pulse: Joi.number().required(),
    spO2 : Joi.number().required(),
+
    cough  : Joi.boolean().required(),
+   runnyNose: Joi.boolean().required(),
+   redEye: Joi.boolean().required(),
+   rash : Joi.boolean().required(),
    soreThroat : Joi.boolean().required(),
+   canNotSmell: Joi.boolean().required(),
+   canNotTaste: Joi.boolean().required(),
+   canBreathRegularly: Joi.boolean().required(),//หายใจปกติไม่เหนื่อยหอบ
+
+   diarrhoeaMoreThan3: Joi.boolean().required(),
+   tired: Joi.boolean().required(),
+   stuffyChest: Joi.boolean().required(),
+   nausea: Joi.boolean().required(),
+   
+   chestHurt: Joi.boolean().required(),
+   slowResponse: Joi.boolean().required(),
+
+
    headAche: Joi.boolean().required(),
    hasHelper : Joi.boolean().required(),
 })
