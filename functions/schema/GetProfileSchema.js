09231d0eb1abb0e2e001df598ca8f9a366e125dd
new5558 <new17353@gmail.com>
2021-08-02 16:59:43 +0700

    fix: wrong getProfile schema

--- functions/schema/GetProfileSchema.js
+++ functions/schema/GetProfileSchema.js
@@ -2,5 +2,5 @@ const Joi = require("joi");
 
 module.exports = Joi.object({
   lineIDToken: Joi.string().required(), // TODO: get from authentication
-  userID: Joi.number().required(),
+  userID: Joi.string().required(),
 });
