8e5bc49d486a9666d32624357b60509048cbc2ca
pie31267 <pie31267@gmail.com>
2021-08-04 02:47:51 +0700

    fix variable names

--- functions/index.js
+++ functions/index.js
@@ -45,19 +45,6 @@ initializeApp();
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
-exports.addMessage = functions
-  .region(region)
-  .https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin
-      .firestore()
-      .collection("messages")
-      .add({ original: original });
-    // Send back a message that we've successfully written the message
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-  });
 
 exports.registerParticipant = functions
   .region(region)
@@ -104,13 +91,13 @@ exports.registerParticipant = functions
     if (snapshot.exists) {
       throw new functions.https.HttpsError(
         "already-exists",
-        `มีข้อมูลผู้ใช้ ${lineId} ในระบบแล้ว`
+        `มีข้อมูลผู้ใช้ ${lineUserID} ในระบบแล้ว`
       )
     }
 
     await snapshot.ref.create(obj)
 
-    return success(`Registration with ID: ${lineId} added`);
+    return success(`Registration with ID: ${lineUserID} added`);
   });
 
 exports.getProfile = functions.region(region).https.onCall(async (data, _) => {
