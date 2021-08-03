5c1c1823f75e9d1165ca78778024a843e7d7f409
pie31267 <pie31267@gmail.com>
2021-08-04 03:22:44 +0700

    remove export patient

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
-const { exportPatient, convertTZ } = require("./utils");
+const { convertTZ } = require("./utils");
 const {
   authenticateVolunteer,
   getProfile,
