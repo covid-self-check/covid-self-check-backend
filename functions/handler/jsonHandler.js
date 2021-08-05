9570ee7299ffdd7a19097d47601f04eb6f2bbb8f
pie31267 <pie31267@gmail.com>
2021-08-06 01:14:57 +0700

    add info pics and edit greeting message

--- functions/handler/jsonHandler.js
+++ functions/handler/jsonHandler.js
@@ -6,10 +6,13 @@ const symptomDiagnostic = require("../json/symptomDiagnostic.json");
 const info1 = require("../json/info1.json");
 const info2 = require("../json/info2.json");
 const info3 = require("../json/info3.json");
+const info4 = require("../json/info4.json");
+const info5 = require("../json/info3.json");
+const info6 = require("../json/info6.json");
 const defaultReply = require("../json/defaultReply.json");
 const tutorial = require("../json/tutorial.json");
 const requestCall = require("../linefunctions/requestCallHandler");
 
-const map = { greeting, welcomepos1, welcomepos2, help, symptomDiagnostic, info1, info2, info3, defaultReply, tutorial, requestCall };
+const map = { greeting, welcomepos1, welcomepos2, help, symptomDiagnostic, info1, info2, info3,info4, info5, info6, defaultReply, tutorial, requestCall };
 
 module.exports = { jsonController: (json) => map[json] };
