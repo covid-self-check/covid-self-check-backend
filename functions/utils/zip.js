e027b2a990a4c546a5e764dbbfe48f39edef6418
palsp <palsp2406@gmail.com>
2021-08-05 14:01:14 +0700

    feat: generalize zip function

--- functions/utils/zip.js
+++ functions/utils/zip.js
@@ -3,47 +3,13 @@ const JSZip = require("jszip");
 
 /**
  * generate multiple csv file and send zip file back to client
- * @param {Express.Response} res
  * @param {number} size - number of volunteer
  * @param {data} data - snapshot from firebase (need to convert to array of obj)
+ * @param {string[]} headers - array of headers
+ * @param {() => any[]} serializer - function that return element of each row
  */
-const generateZipFile = (res, size, data) => {
-  const arrs = _.chunk(data, size);
-
-  const zip = new JSZip();
-
-  arrs.forEach((arr, i) => {
-    const aoa = convertToAoA(arr);
-    const filename = `${i + 1}.csv`;
-    const ws = XLSX.utils.aoa_to_sheet(aoa);
-
-    const csv = XLSX.utils.sheet_to_csv(ws, { RS: "\n" });
-    zip.file(filename, csv);
-  });
-
-  zip
-    .generateAsync({ type: "base64" })
-    .then(function (content) {
-      res.json({
-        title: "report.zip",
-        content: content,
-      });
-    })
-    .catch((err) => {
-      res.json({
-        err,
-      });
-    });
-};
-
-/**
- * generate multiple csv file and send zip file back to client
- * @param {number} size - number of volunteer
- * @param {data} data - snapshot from firebase (need to convert to array of obj)
- */
-exports.generateZipFileRoundRobin = async (size, data) => {
-  var arrs = new Array(size);
-  const header = ["internal id", "first name", "call status", "tel"];
+exports.generateZipFileRoundRobin = async (size, data, headers, serializer) => {
+  const arrs = new Array(size);
 
   for (let i = 0; i < size; i++) {
     arrs[i] = [];
@@ -55,14 +21,9 @@ exports.generateZipFileRoundRobin = async (size, data) => {
 
   const zip = new JSZip();
   arrs.forEach((arr, i) => {
-    const aoa = [[...header]];
-    arr.forEach((data) => {
-      aoa.push([
-        data.id,
-        data.firstName,
-        data.hasCalled,
-        `="${data.personalPhoneNo}"`,
-      ]);
+    const aoa = [[...headers]];
+    arr.forEach((el) => {
+      aoa.push(serializer(el));
     });
 
     const filename = `${i + 1}.csv`;
