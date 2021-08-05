094b63ed56b9e69d673d71b50b821dfb9d25ab77
new5558 <new17353@gmail.com>
2021-08-06 01:43:38 +0700

    fix: patient color count not working

--- functions/controller/patientController.js
+++ functions/controller/patientController.js
@@ -4,7 +4,7 @@ const { admin } = require('../init')
 const { getProfile } = require('../middleware/authentication')
 const { convertTZ } = require('../utils')
 const { success } = require('../response/success')
-const { makeStatusAPIPayload, makeRequest, statusList } = require('../api/api')
+const { makeStatusAPIPayload, makeRequest, statusList, statusListReverse } = require('../api/api')
 const { sendPatientstatus } = require('../linefunctions/linepushmessage')
 const { notifyToLine } = require('../linenotify')
 const { convertTimestampToStr } = require('../utils/date')
@@ -35,7 +35,7 @@ const addTotalPatientCountByColor = async (status) => {
     .doc(status)
     .get()
 
-  await snapshot.ref('count', admin.firestore.FieldValue.increment(1))
+  await snapshot.ref.update('count', admin.firestore.FieldValue.increment(1))
 }
 
 const decrementTotalPatientCountByColor = async (status) => {
@@ -45,7 +45,7 @@ const decrementTotalPatientCountByColor = async (status) => {
     .doc(status)
     .get()
 
-  await snapshot.ref('count', admin.firestore.FieldValue.increment(-1))
+  await snapshot.ref.update('count', admin.firestore.FieldValue.increment(-1))
 }
 
 exports.registerPatient = async (data, _context) => {
@@ -260,7 +260,7 @@ exports.updateSymptom = async (data, _context) => {
 
   try {
     if (previousStatus !== null) {
-      await decrementTotalPatientCountByColor(previousStatus)
+      await decrementTotalPatientCountByColor(statusListReverse[previousStatus])
     }
 
     if (obj['toAmed'] === 1) {
