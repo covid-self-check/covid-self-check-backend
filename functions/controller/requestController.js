ee2e1cd38ff8ef593a9c4835a8cd94556d508a5d
palsp <palsp2406@gmail.com>
2021-08-05 21:49:24 +0700

    feat: block amend user from r2c

--- functions/controller/requestController.js
+++ functions/controller/requestController.js
@@ -31,6 +31,9 @@ exports.requestToCall = async (data, _context) => {
     .doc(lineUserID)
     .get();
   if (!snapshot.exists) {
+    if (snapshot.data().toAmed === 1) {
+      success(`your information already handle by Amed`);
+    }
     throw new functions.https.HttpsError(
       "not-found",
       `ไม่พบผู้ใช้ ${lineUserID}`
