7d2dc13cdd8a1f732ef12520208953dd9ea00d25
guynawapas <guynawapas@gmail.com>
2021-08-06 16:41:10 +0700

    register done w/o mockdb

--- functions/controller/patientController/utils.test.js
+++ functions/controller/patientController/utils.test.js
@@ -1,5 +1,6 @@
-const { setPatientStatus } = require("./utils");
+const { setPatientStatus, snapshotExists, addCreatedDate } = require("./utils");
 const { admin } = require("../../init");
+const functions = require("firebase-functions");
 
 describe("setPatientStatus", () => {
   it("should setPatientStatus correctly", () => {
@@ -16,6 +17,36 @@ describe("setPatientStatus", () => {
       isRequestToCall: false,
       isNurseExported: false,
       toAmed: 0,
+    });
+  });
+});
+
+describe("snapshotExists", () => {
+  it("throw amed", () => {
+    function checkExists() {
+      const mockSnapshot = { exists: true, data: () => ({ toAmed: 1 }) };
+      snapshotExists(mockSnapshot);
+    }
+    expect(checkExists).toThrowError(
+      "your information is already handle by Amed"
+    );
+  });
+  it("throw มีข้อมูลแล้ว", () => {
+    function checkExists() {
+      const mockSnapshot = { exists: true, data: () => ({ toAmed: 0 }) };
+      snapshotExists(mockSnapshot);
+    }
+    expect(checkExists).toThrowError("มีข้อมูลผู้ใช้ในระบบแล้ว");
+  });
+});
+
+describe("addCreatedDate", () => {
+  it("should add createdDate correctly", () => {
+    const mockObj = {};
+    const createdDate = new Date();
+    addCreatedDate(mockObj, createdDate);
+    expect(mockObj).toEqual({
+      createdDate: admin.firestore.Timestamp.fromDate(createdDate),
     });
   });
 });
