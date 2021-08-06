9ef08c8aca94cb3888e27b92f0b507015a14ba26
new5558 <new17353@gmail.com>
2021-08-06 16:39:03 +0700

    fix: check again

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -1,9 +1,16 @@
 const { convertTZ } = require("./date");
+const moment = require("moment-timezone");
 
 describe("convertTZ", () => {
   it("should convert same timezone correctly", () => {
     // const now = new Date("2021-08-06T07:54:08 GMT+0800");
-    const now = new Date("Feb 28 2013 19:00:00 GMT-0500");
+    // const now = new Date("Feb 28 2013 19:00:00 GMT+7500");
+    // const now = moment('"2021-08-06T07:54:08').tz("Asia/Bangkok").toDate();
+    const now = moment
+      .tz("May 12th 2014 8AM", "MMM Do YYYY hA", "Asia/Bangkok")
+      .toDate();
+    // const now = moment('08/06/2021 07:54:08').tz("Asia/Bangkok").toDate();
+    console.log(now, "now");
     const result = convertTZ(now, "Asia/Bangkok");
     // console.log(now.toString());
     // console.log(now.toISOString());
@@ -15,9 +22,14 @@ describe("convertTZ", () => {
   });
 
   it("should convert different timezone correctly", () => {
-    const now = new Date("2021-08-06T01:54:08+0700");
+    // const now = new Date("2021-08-06T01:54:08+0700");
+    // const now = moment('"2021-08-06T01:54:08').tz("Asia/Bangkok").toDate();
+    // const now = moment('08/06/2021 07:54:08').tz("Asia/Bangkok").toDate();
+    const now = moment
+      .tz("May 12th 2014 3AM", "MMM Do YYYY hA", "Asia/Bangkok")
+      .toDate();
     const result = convertTZ(now, "Africa/Accra");
-    expect(result.getDate()).toEqual(5);
-    expect(result.getHours()).toEqual(18);
+    expect(result.getDate()).toEqual(11);
+    expect(result.getHours()).toEqual(20);
   });
 });
