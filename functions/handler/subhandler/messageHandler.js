fe2aca462af3a1385e53df45ac0926e3690dc45d
new5558 <new17353@gmail.com>
2021-08-06 14:27:34 +0700

    fix: format with prettier

--- functions/handler/subhandler/messageHandler.js
+++ functions/handler/subhandler/messageHandler.js
@@ -1,34 +1,46 @@
-const { jsonController } = require('../jsonHandler')
-const { requestCall } = require('../../linefunctions/requestCallHandler')
+const { jsonController } = require("../jsonHandler");
+const { requestCall } = require("../../linefunctions/requestCallHandler");
 
 const handleMessage = async (event, userObject, client) => {
-    const replyToken = await event.replyToken
-    const message = await event.message.text
-    // console.log(message)
-    try {
-        switch (message) {
-            case 'ประเมินอาการ':
-                await client.replyMessage(replyToken, jsonController('symptomDiagnostic'))
-                break;
-            case 'สิ่งที่ควรรู้':
-                await client.replyMessage(replyToken, [jsonController('info1'), jsonController('info2'), jsonController('info3'), jsonController('info4'), jsonController('info6')])
-                break;
-            case 'ติดต่อฉุกเฉิน':
-                await client.replyMessage(replyToken, jsonController('help'))
-                break;
-            case 'สอนการใช้งาน':
-                await requestCall(userObject, client, replyToken)
-                break;
-            default:
-                await client.replyMessage(replyToken,jsonController('defaultReply'))
-                break;
-        }
-    } catch (error) {
-        console.log(error);
-        await client.replyMessage(replyToken, { type: "text", text: "ERROR, please report admin !" });
+  const replyToken = await event.replyToken;
+  const message = await event.message.text;
+  // console.log(message)
+  try {
+    switch (message) {
+      case "ประเมินอาการ":
+        await client.replyMessage(
+          replyToken,
+          jsonController("symptomDiagnostic")
+        );
+        break;
+      case "สิ่งที่ควรรู้":
+        await client.replyMessage(replyToken, [
+          jsonController("info1"),
+          jsonController("info2"),
+          jsonController("info3"),
+          jsonController("info4"),
+          jsonController("info6"),
+        ]);
+        break;
+      case "ติดต่อฉุกเฉิน":
+        await client.replyMessage(replyToken, jsonController("help"));
+        break;
+      case "สอนการใช้งาน":
+        await requestCall(userObject, client, replyToken);
+        break;
+      default:
+        await client.replyMessage(replyToken, jsonController("defaultReply"));
+        break;
     }
-}
+  } catch (error) {
+    console.log(error);
+    await client.replyMessage(replyToken, {
+      type: "text",
+      text: "ERROR, please report admin !",
+    });
+  }
+};
 
 module.exports = {
-    handleMessage,
+  handleMessage,
 };
