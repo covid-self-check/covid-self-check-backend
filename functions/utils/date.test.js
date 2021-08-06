54889f0dcdfe2a403df75ddca704742973f1b578
new5558 <new17353@gmail.com>
2021-08-06 15:42:58 +0700

    fix: log more

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -4,7 +4,9 @@ describe("convertTZ", () => {
   it("should convert timezone correctly", () => {
     const now = new Date("2021-08-06T07:54:08+07:00");
     const result = convertTZ(now, "Asia/Bangkok");
+    console.log("now.toString()");
     console.log(now.toString());
+    console.log("result.toString()");
     console.log(result.toString());
     expect(result.getDate()).toEqual(now.getDate());
     expect(result.getHours()).toEqual(now.getHours());
