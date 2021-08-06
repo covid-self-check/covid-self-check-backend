39a92137372c1cc797d4fa16d94bacd42fac14b0
new5558 <new17353@gmail.com>
2021-08-06 15:53:08 +0700

    fix: to iso string

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -1,18 +1,18 @@
 const { convertTZ } = require("./date");
 
 describe("convertTZ", () => {
-  it("should convert timezone correctly", () => {
-    const now = new Date("2021-08-06T07:54:08+07:00");
+  it("should convert same timezone correctly", () => {
+    const now = new Date("2021-08-06T07:54:08+09:00");
     const result = convertTZ(now, "Asia/Bangkok");
     console.log("now.toString()");
-    console.log(now.toString());
+    console.log(now.toISOString());
     console.log("result.toString()");
-    console.log(result.toString());
+    console.log(result.toISOString());
     expect(result.getDate()).toEqual(now.getDate());
     expect(result.getHours()).toEqual(now.getHours());
   });
 
-  it("should convert timezone correctly", () => {
+  it("should convert different timezone correctly", () => {
     const now = new Date("2021-08-06T20:54:08+07:00");
     const result = convertTZ(now, "Africa/Accra");
     expect(result.getDate()).toEqual(6);
