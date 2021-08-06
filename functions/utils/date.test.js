e3963f8ce0587ff2283ec33c283e6a8911998b48
new5558 <new17353@gmail.com>
2021-08-06 16:28:07 +0700

    test: change test method

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -2,7 +2,8 @@ const { convertTZ } = require("./date");
 
 describe("convertTZ", () => {
   it("should convert same timezone correctly", () => {
-    const now = new Date("2021-08-06T07:54:08+07:00");
+    // const now = new Date("2021-08-06T07:54:08 GMT+0800");
+    const now = new Date("Feb 28 2013 19:00:00 GMT-0500");
     const result = convertTZ(now, "Asia/Bangkok");
     // console.log(now.toString());
     // console.log(now.toISOString());
@@ -14,7 +15,7 @@ describe("convertTZ", () => {
   });
 
   it("should convert different timezone correctly", () => {
-    const now = new Date("2021-08-06T01:54:08+07:00");
+    const now = new Date("2021-08-06T01:54:08+0700");
     const result = convertTZ(now, "Africa/Accra");
     expect(result.getDate()).toEqual(5);
     expect(result.getHours()).toEqual(18);
