b86ee83d64bd071ad7edf06a358d3e79afc74252
new5558 <new17353@gmail.com>
2021-08-06 17:06:26 +0700

    fix convert date 4

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -12,7 +12,7 @@ describe("convertTZ", () => {
   });
 
   it("should convert same timezone correctly", () => {
-    const now = new Date("2021-08-06T14:54:08");
+    const now = new Date("2021-08-06T14:54:08+07:00");
     const result = convertTZ(now, "Asia/Bangkok");
     // console.log(now.toString());
     // console.log(now.toISOString());
@@ -24,14 +24,14 @@ describe("convertTZ", () => {
   });
 
   it("should convert different timezone correctly", () => {
-    const now = new Date("2021-08-06T01:54:08");
+    const now = new Date("2021-08-06T01:54:08+07:00");
     const result = convertTZ(now, "Africa/Accra");
     expect(result.getDate()).toEqual(5);
     expect(result.getHours()).toEqual(18);
   });
 
   it("should convert same string timezone correctly", () => {
-    const stringTime = "2021-08-06T14:54:08";
+    const stringTime = "2021-08-06T14:54:08+07:00";
     const now = new Date(stringTime);
     const result = convertTZ(stringTime, "Asia/Bangkok");
     expect(result.getDate()).toEqual(now.getDate());
@@ -39,7 +39,7 @@ describe("convertTZ", () => {
   });
 
   it("should convert different string timezone correctly", () => {
-    const result = convertTZ("2021-08-06T01:54:08", "Africa/Accra");
+    const result = convertTZ("2021-08-06T01:54:08+07:00", "Africa/Accra");
     expect(result.getDate()).toEqual(5);
     expect(result.getHours()).toEqual(18);
   });
