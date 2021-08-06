9d1d3808ba10e288a57b274525a70508a39b2d4d
new5558 <new17353@gmail.com>
2021-08-06 15:55:54 +0700

    fix: log test

--- functions/utils/date.test.js
+++ functions/utils/date.test.js
@@ -2,18 +2,18 @@ const { convertTZ } = require("./date");
 
 describe("convertTZ", () => {
   it("should convert same timezone correctly", () => {
-    const now = new Date("2021-08-06T07:54:08+09:00");
+    const now = new Date("2021-08-06T07:54:08");
     const result = convertTZ(now, "Asia/Bangkok");
-    console.log("now.toString()");
+    console.log(now.toString());
     console.log(now.toISOString());
-    console.log("result.toString()");
+    console.log(result.toString());
     console.log(result.toISOString());
     expect(result.getDate()).toEqual(now.getDate());
     expect(result.getHours()).toEqual(now.getHours());
   });
 
   it("should convert different timezone correctly", () => {
-    const now = new Date("2021-08-06T20:54:08+07:00");
+    const now = new Date("2021-08-06T20:54:08");
     const result = convertTZ(now, "Africa/Accra");
     expect(result.getDate()).toEqual(6);
     expect(result.getHours()).toEqual(13);
