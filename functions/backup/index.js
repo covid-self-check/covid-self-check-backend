754a98a940e62f25f549aacc737b4737bd926e6c
guynawapas <guynawapas@gmail.com>
2021-08-08 14:30:46 +0700

    add check on backup

--- functions/backup/index.js
+++ functions/backup/index.js
@@ -7,6 +7,7 @@ const { config } = require("../config/index");
 const { google } = require("googleapis");
 
 const privateKey = _.replace(config.backupAccount.privateKey, /\\n/g, "\n");
+const isDevelopment = functions.config().environment.isDevelopment;
 
 const authClient = new google.auth.JWT({
   email: config.backupAccount.clientEmail,
@@ -24,6 +25,7 @@ const firestoreClient = google.firestore({
 });
 
 exports.backup = async (context) => {
+  if (isDevelopment) return;
   // TODO: get project ID from env
   const projectId = admin.instanceId().app.options.projectId;
 
