bfaef5d59278a92a20de420aa215651c0b8d4c05
latte34844 <latte34844@gmail.com>
2021-08-20 21:29:36 +0700

    updatedb userbtw36hrsto72hrs

--- functions/controller/pubsub/index.js
+++ functions/controller/pubsub/index.js
@@ -1,3 +1,4 @@
+const utils = require("./utils");
 const { admin, collection } = require("../../init");
 const { getDateID } = require("../../utils/date");
 
@@ -12,3 +13,19 @@ exports.initializeR2CStat = async (_context) => {
     await snapshot.ref.create({ count: 0 });
   }
 };
+
+exports.updatenumberuserbtw36hrsto72hrs = async () => {
+  const notUpdatedlistnumber =  await utils.getnumberusersbtw36hrsto72hrs();
+  const id = getDateID();
+  const snapshot = await admin
+    .firestore()
+    .collection(collection.usersbtw36hrsto72hrs)
+    .doc(id)
+    .get();
+  if (!snapshot.exists) {
+    await snapshot.ref.create({ count: notUpdatedlistnumber });
+  }
+  // return(notUpdatedlistnumber);
+}
+
+
