e3b4aadc7b8c6d4a7a227dea39491ea189930e1c
new5558 <new17353@gmail.com>
2021-08-20 19:54:13 +0700

    fix: delete lib folder

--- functions/lib/init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// The Firebase Admin SDK to access Firestore.
-const admin = require("firebase-admin");
-exports.initializeApp = function () {
-  admin.initializeApp();
-};
-exports.admin = admin;
-//# sourceMappingURL=init.js.map
