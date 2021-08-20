ba2e63af4a593f1e2aa04bfe61a489eb49adf81b
palsp <palsp2406@gmail.com>
2021-08-20 22:35:30 +0700

    solve conflict

--- functions/src/controller/pubsub/index.js
+++ functions/src/controller/pubsub/index.js
@@ -72,3 +72,17 @@ exports.initializeLegacyStat = async (_context) => {
     .doc("stat")
     .set({ count: snapshot.docs.length });
 };
+
+exports.updatenumberuserbtw36hrsto72hrs = async () => {
+  const notUpdatedlistnumber = await utils.getnumberusersbtw36hrsto72hrs();
+  const id = getDateID();
+  const snapshot = await admin
+    .firestore()
+    .collection(collection.usersbtw36hrsto72hrs)
+    .doc(id)
+    .get();
+  if (!snapshot.exists) {
+    await snapshot.ref.create({ count: notUpdatedlistnumber });
+  }
+  // return(notUpdatedlistnumber);
+};
