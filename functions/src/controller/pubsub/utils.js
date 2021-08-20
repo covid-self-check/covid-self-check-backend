5f18b9749f1dbb6b572bafa08a34b595a2b8c4c0
palsp <palsp2406@gmail.com>
2021-08-20 22:35:30 +0700

    solve conflict

--- functions/src/controller/pubsub/utils.js
+++ functions/src/controller/pubsub/utils.js
@@ -1,3 +1,10 @@
+const { get36hrsUsers } = require("../exportController/utils");
+
 exports.calculateHours = (currentDate, lastUpdatedDate) => {
   return Math.abs(currentDate - lastUpdatedDate) / 36e5;
+};
+
+exports.getnumberusersbtw36hrsto72hrs = async () => {
+  const temp_notUpdatedList = await get36hrsUsers();
+  return temp_notUpdatedList.length;
 };
