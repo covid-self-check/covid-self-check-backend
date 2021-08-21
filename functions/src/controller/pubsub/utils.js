8aed017b8b7aaacb8b1abddc823d6abbc7187344
latte34844 <latte34844@gmail.com>
2021-08-21 21:33:30 +0700

    timeseries collection v1

--- functions/src/controller/pubsub/utils.js
+++ functions/src/controller/pubsub/utils.js
@@ -1,4 +1,5 @@
 const { get36hrsUsers } = require("../exportController/utils");
+const { statusList } = require("../../api/const");
 
 exports.calculateHours = (currentDate, lastUpdatedDate) => {
   return Math.abs(currentDate - lastUpdatedDate) / 36e5;
@@ -8,3 +9,33 @@ exports.getnumberusersbtw36hrsto72hrs = async () => {
   const temp_notUpdatedList = await get36hrsUsers();
   return temp_notUpdatedList.length;
 };
+
+exports.getActiveUser = async ()=>{
+  const snapshot = await admin.firestore().collection(collection.patient).get();
+
+  const notUpdatedList = [];
+  const currentDate = new Date();
+  snapshot.forEach((doc) => {
+    const patient = doc.data();
+
+    const lastUpdatedDate = patient.lastUpdatedAt.toDate();
+    var hours = Math.abs(currentDate - lastUpdatedDate) / 36e5;
+    const includeStatus = [
+      statusList["unknown"],
+      statusList["G1"],
+      statusList["G2"],
+    ];
+
+    if (includeStatus.includes(patient.status)) {
+      if (hours <= 36) {
+        notUpdatedList.push({
+          firstName: patient.firstName,
+          personalPhoneNo: patient.personalPhoneNo,
+        });
+      }
+    }
+  });
+  return notUpdatedList.length;
+}
+
+
