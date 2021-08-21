d9f6222c58abdd91b95a7c145007f8b83491e514
palsp <palsp2406@gmail.com>
2021-08-21 16:14:12 +0700

    feat: add onUpdate patient

--- functions/src/controller/firestoreController/index.ts
+++ functions/src/controller/firestoreController/index.ts
@@ -1,4 +1,4 @@
-import { OnCreateHandler, Patient } from "../../types"
+import { OnCreateHandler, OnUpdateHandler, Patient } from "../../types"
 import { admin } from "../../init"
 import * as utils from "./utils"
 import { statusListReverse } from "../../api/const"
@@ -20,6 +20,26 @@ export const onRegisterPatient: OnCreateHandler<Patient> = async (snapshot, _con
     console.log(e)
   }
 }
+
+export const onUpdatePatient: OnUpdateHandler<Patient> = async (change, context) => {
+  try {
+    const batch = admin.firestore().batch();
+
+    const prevData = change.before.data();
+    const currentData = change.after.data();
+
+    // decrement from old color
+    await utils.decrementTotalPatientCountByStatus(batch, statusListReverse[prevData.status])
+
+    // increment new color
+
+    await utils.incrementTotalPatientCountByStatus(batch, statusListReverse[currentData.status])
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 
 
 
