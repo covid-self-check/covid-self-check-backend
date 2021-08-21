4d81b7a1ac55b1b5d4f0c2b305d8547f1dccad35
palsp <palsp2406@gmail.com>
2021-08-21 12:33:52 +0700

    feat: add onCreate and patient type

--- functions/src/controller/firestoreController/index.ts
+++ functions/src/controller/firestoreController/index.ts
@@ -1,10 +1,11 @@
-import { OnCreateHandler } from "../../types"
+import { OnCreateHandler, Patient } from "../../types"
 import { admin } from "../../init"
 import * as utils from "./utils"
 import { statusListReverse } from "../../api/const"
 
 
-export const onRegisterPatient: OnCreateHandler = async (snapshot, context) => {
+
+export const onRegisterPatient: OnCreateHandler<Patient> = async (snapshot, _context) => {
   try {
     const batch = admin.firestore().batch();
     // snapshot.status
