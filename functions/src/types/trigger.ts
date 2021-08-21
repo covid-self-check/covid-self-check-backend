3e6d84cd7972f6d96083ed623071a6bc1896e568
palsp <palsp2406@gmail.com>
2021-08-21 16:14:12 +0700

    feat: add onUpdate patient

--- functions/src/types/trigger.ts
+++ functions/src/types/trigger.ts
@@ -1,6 +1,7 @@
 import { QueryDocumentSnapshot } from "@google-cloud/firestore";
-import { WriteBatch, DocumentSnapshot, DocumentReference } from "@google-cloud/firestore"
-import { EventContext } from "firebase-functions";
+import { WriteBatch, DocumentSnapshot, DocumentReference, DocumentData } from "@google-cloud/firestore"
+import { EventContext, Change } from "firebase-functions";
 
-export type OnCreateHandler<T> = (snapshot: QueryDocumentSnapshot<T>, context: EventContext) => PromiseLike<any> | any
+export type OnCreateHandler<T = DocumentData> = (snapshot: QueryDocumentSnapshot<T>, context: EventContext) => PromiseLike<any> | any
 export type PatientCountHandler = (snapshot: DocumentSnapshot, ref: DocumentReference, batch: WriteBatch) => void
+export type OnUpdateHandler<T = DocumentData> = (change: Change<QueryDocumentSnapshot<T>>, context: EventContext) => PromiseLike<any> | any
