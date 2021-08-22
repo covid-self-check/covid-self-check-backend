8ef9472c671ba9ab2c8c3a43a29831d54ceac075
latte34844 <latte34844@gmail.com>
2021-08-22 20:52:36 +0700

    accumulative data v3

--- functions/src/controller/dashboard/index.js
+++ functions/src/controller/dashboard/index.js
@@ -1,35 +1,30 @@
-const { RSA_X931_PADDING } = require("constants");
 const { admin, collection } = require("../../init");
 
 exports.getAccumulative = async() => {
-    const typeDocs = ["G1","G2","R1","R2","Y1","Y2","users"];
-    const tempNum = [];
-    for(let i=0; i<typeDocs.length;i++){
-        const docRef = await admin
-          .firestore()
-          .collection(collection.userCount)
-          .doc(typeDocs[i])
-          .get();
-        if(docRef.exists){
-            tempNum.push(docRef.data().count);  
-        } else {
-            tempNum.push(0);
-        }            
-    };
-    const legacyRef = await admin
-      .firestore()
-      .collection(collection.legacyStat)
-      .doc("stat")
-      .get();
+    const [g1,g2,r1,r2,y1,y2,users,unknown,legacyRef] = await Promise.all([getCount(collection.userCount,"G1"),getCount(collection.userCount,"G2"),getCount(collection.userCount,"R1"),getCount(collection.userCount,"R2"),getCount(collection.userCount,"Y1"),getCount(collection.userCount,"Y2"),getCount(collection.userCount,"users"),getCount(collection.userCount,"unknown"),getCount(collection.legacyStat,"stat")]);
     const temp = {
-        G1:tempNum[0],
-        G2:tempNum[1],
-        R1:tempNum[2],
-        R2:tempNum[3],
-        Y1:tempNum[4],
-        Y2:tempNum[5],
-        users:tempNum[6],
-        legacyCount: legacyRef.data().count
+        G1:g1,
+        G2:g2,
+        R1:r1,
+        R2:r2,
+        Y1:y1,
+        Y2:y2,
+        users:users,
+        unknown:unknown,
+        legacyCount: legacyRef
     };
     return temp;
 };
+
+const getCount = async(collection, document) =>{
+    const docRef = await admin
+          .firestore()
+          .collection(collection)
+          .doc(document)
+          .get();
+    if(docRef.exists){
+        return docRef.data().count;
+    }else{
+        return 0;
+    }
+}
