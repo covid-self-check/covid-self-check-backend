c95973782075bbe281fc558e94d96de7d79efbf9
palsp <palsp2406@gmail.com>
2021-09-11 10:59:32 +0700

    feat: remove export 36 hr

--- functions/src/types/patient.ts
+++ functions/src/types/patient.ts
@@ -1,21 +1,11 @@
 import { Timestamp } from "@google-cloud/firestore"
-import { HistoryType, RegisterType } from '../schema'
+import { HistoryType } from '../schema'
 
 export type FollowUp = Omit<HistoryType, "noAuth" | "lineIDToken" | "lineUserID">
 
-export type Patient = Partial<FollowUp> & {
-  followUp: FollowUp[]
-  status: number
-  needFollowUp: boolean
-  createdDate: Timestamp
-  lastUpdatedAt: Timestamp
-  isRequestToCallExported: boolean
-  isRequestToCall: boolean
-  isNurseExported: boolean
-  toAmed: number
-  birthDate: Timestamp
-} & Omit<RegisterType, "noAuth" | "lineIDToken" | "lineUserID" | "birthDate">
-
+export type Patient = {
+  followUp: UpdatedPatient[]
+}
 
 export type UpdatedPatient = {
   status: number
