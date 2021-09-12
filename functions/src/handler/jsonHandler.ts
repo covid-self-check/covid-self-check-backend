bc20342a7beb8fa38287be2079dd2b0fd792e3b4
Ravipas Wangananont <pestobaimon@gmail.com>
2021-09-12 16:11:52 +0700

    added FAQ and symptom carousel

--- functions/src/handler/jsonHandler.ts
+++ functions/src/handler/jsonHandler.ts
@@ -1,6 +1,6 @@
-import * as greeting from "../json/greeting.json";
+import * as greeting from "../json/others/greeting.json";
 import * as welcomepos1 from "../json/welcomepos1.json";
-import * as help from "../json/help.json";
+import * as help from "../json/emergency/help.json";
 import * as symptomDiagnostic from "../json/symptomDiagnostic.json";
 import * as info1 from "../json/info1.json";
 import * as info2 from "../json/info2.json";
@@ -8,8 +8,12 @@ import * as info3 from "../json/info3.json";
 import * as info4 from "../json/info4.json";
 import * as info5 from "../json/info5.json";
 import * as info6 from "../json/info6.json";
-import * as defaultReply from "../json/defaultReply.json";
+import * as defaultReply from "../json/others/defaultReply.json";
 import * as tutorial1 from "../json/tutorial1.json";
+
+import * as faqCarousel from "../json/faq/faqCarousel.json";
+
+import * as SymptomCarousel from "../json/symptom/symptomCarousel.json"; 
 
 import * as guide from "../json/guide.json";
 import * as r2cQuestion from "../json/r2cQuestion.json";
@@ -36,6 +40,8 @@ const map: { [key: string]: any } = {
   greeting,
   welcomepos1,
   help,
+  faqCarousel,
+  SymptomCarousel,
   symptomDiagnostic,
   info1,
   info2,
